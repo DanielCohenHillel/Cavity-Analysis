3afbe37d4f2cd1b7345dbd30128159c360bb96a7
Daniel Cohen Hillel <danielgch98@gmail.com>
2020-07-14 23:16:25 +0300

    Added a more detatiled analysis of the dephasing
    Also updated the README

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # Cavity Analysis
+
 Analysis of the transmon-cavity system used in our lab.
 
 ## Half vs Full
-Comparison of a half Tesla cavity to a full Tesla cavity. Checking for quality factors, life-times and dissipation using both a volume
-integral method (modeling the dirt as a 3D object) and surface integral method (integrting the surface of the cavity to analyze the dirt).
+
+Comparison of a half Tesla cavity to a full Tesla cavity. Checking for quality
+factors, life-times and dissipation using both a volume integral method
+(modeling the dirt as a 3D object) and surface integral method (integrting the
+surface of the cavity to analyze the dirt).
 
 ## Coupling
-Analysis of the copuling between the transom and the readout strip-line.
+
+Analysis of the coupling between the transom and the readout strip-line.
 
 **Note:** For this to work you need pyEPR version from after 19/05/2020 (see [this commit](https://github.com/zlatko-minev/pyEPR/commit/29ee909e1fa2c0ddd879afac4ca90123098b8baf)).
-.
+
+## Chip Test
+
+GDS design of chip used to test the cavity with a coil instead of a real transmon.
+
+## Dephasing
+
+Simulations of method to improve the dephasing time of our the cavity.
--- dephasing/tools.py
+++ dephasing/tools.py
@@ -104,20 +104,24 @@ params = Params(
 
 
 def proj(state, cavity_levels, mu=0, rnd=False):
-    """
+    '''
     Projecting the qubit into the ground state while keeping the cavity state the same
 
     State(qobj) - The cavity-transmon state to prject
     mu(float)   - The error rate (0-no errors  1-all errors)
     rnd(bool)   - Weather to use density matrix probabilites for the errors or do them randomly (True=do randomly)
-    """
+    '''
     g = qt.basis(qub_lvls, 0)
     e = qt.basis(qub_lvls, 1)
+    f = qt.basis(qub_lvls, 2)
     N = qt.qeye(cavity_levels)
     N_gg = qt.tensor(N, g*g.dag())  # |N> X |g><g|
     N_ge = qt.tensor(N, g*e.dag())  # |N> X |g><e|
     N_eg = qt.tensor(N, e*g.dag())  # |N> X |e><g|
     N_ee = qt.tensor(N, e*e.dag())  # |N> X |e><e|
+
+    N_gf = qt.tensor(N, g*f.dag())  # |N> X |g><e|
+    N_fg = qt.tensor(N, f*g.dag())  # |N> X |e><g|
 
     if rnd:
         if np.random.random() > mu:
@@ -126,4 +130,4 @@ def proj(state, cavity_levels, mu=0, rnd=False):
             return N_eg*state*N_ge + N_ee*state*N_ee
 
     # (1-u)*(|g><g| S |g><g|)
-    return (1 - mu)*(N_gg*state*N_gg + N_ge*state*N_eg) + mu*(N_eg*state*N_ge + N_ee*state*N_ee)
+    return (1-mu[0])*(N_gg*state*N_gg + N_ge*state*N_eg + N_gf*state*N_fg) + mu[0]*(N_eg*state*N_ge + N_ee*state*N_ee)
